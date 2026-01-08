a37c7aa28b9254e5e592781dc8ed2aff802434c7
Hunter21422 <vladimirantonos2142@gmail.com>
2026-01-08 16:34:38 +0300

    Update urls.py

--- sixcoffee/urls.py
+++ sixcoffee/urls.py
@@ -1,23 +1,20 @@
+from django.http import JsonResponse
 
-from django.contrib import admin
-from django.conf import settings
-from django.urls import path,include
-from django.conf.urls.static import static
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
-from rest_framework.routers import DefaultRouter
-
-
-router = DefaultRouter()
+def api_root(request):
+    return JsonResponse({
+        "status": "online",
+        "message": "Coffee Loyalty API",
+        "endpoints": {
+            "telegram_auth": "/telegram-auth/",
+            "me": "/me/",
+            "user_profile": "/user/profile/",
+        }
+    })
 
 urlpatterns = [
+    path('', api_root, name='api-root'),
     path('admin/', admin.site.urls),
-    path("", include("Loyality.urls")),
-   
-
+    path(" ", include("Loyality.urls")),
 ]
-# Медиа в DEBUG
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
